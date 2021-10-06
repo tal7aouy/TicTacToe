3ee4a06eeb5e332b735b994ad7ecdf91e312df34
tal7aouy <talhaouycoding@gmail.com>
2021-10-06 20:45:19 +0100

    add darkmode to board

--- js/app.js
+++ js/app.js
@@ -31,12 +31,17 @@ gameView.updateBoard(game)
 // toggle theme
 const btnDarkMode = document.querySelector('#toggle')
 const body = document.querySelector('html')
+const board = document.querySelector('.board')
 btnDarkMode.addEventListener('click', (e) => {
   if (btnDarkMode.checked) {
     body.classList.remove('all')
     body.classList.add('dark')
+    board.classList.remove('all')
+    board.classList.add('dark')
   } else {
     body.classList.add('all')
     body.classList.remove('dark')
+    board.classList.add('all')
+    board.classList.remove('dark')
   }
 })
